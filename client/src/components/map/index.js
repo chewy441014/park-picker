b24e169571368b7d8eef2aa06f2b1c0ba9079dfe
Preston Hill <preston.leon.hill@gmail.com>
2022-12-06 17:04:52 -0600

    added structure of results card, needs formating, added blank map and weather components

---
+++ client/src/components/map/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Map() {
+    return (
+        <div> 
+            {/* This is going to be a little complicated I think, this is where the map goes */}
+        </div>
+    );
+}
+
+export default Map
---
+++ client/src/components/weather/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Weather() {
+    return (
+        <div>
+            {/* Using something similar to 
+              * const numbers = [1, 2, 3, 4, 5];
+              * const listItems = numbers.map((number) =>
+              *   <li>{number}</li>
+              * ); 
+              * display five weather cards */}
+        </div>
+    );
+}
+
+export default Weather
