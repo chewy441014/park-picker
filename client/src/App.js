24d5962c212344634937463254e44d1bbf4b0097
Michael Gostomski <gostomski.m@gmail.com>
2022-12-06 18:39:42 -0600

    search card component

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SearchCard from './components/searchcard';
+
+
+export default function App() {
+  return (
+    <div>
+      <SearchCard />
+    </div>
+  );
+}
---
+++ client/src/components/searchcard/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+function SearchCard() {
+    return (
+        <div>
+            <Form>
+                <Form.Group className="mb-3" controlId="formActivityList">
+                    <Form.Label>Select park activities.</Form.Label>
+                    <Form.Select>
+                        <option>Nothing selected</option>
+                        <option>Camping</option>
+                        <option>Swimming</option>
+                        <option>Hiking</option>
+                    </Form.Select>
+                </Form.Group>
+                <Form.Check
+                    type="switch"
+                    id="custom-switch"
+                    label="Use current location."
+                />
+                <Form.Group className="mb-3" controlId="formLocationSearch">
+                    <Form.Label>Enter your address below</Form.Label>
+                    <Form.Control type="text" placeholder="123 Fake St..." />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </div>
+    );
+}
+
+export default SearchCard
