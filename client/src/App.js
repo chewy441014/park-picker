b0c508af6bb2e42c36410647c72e056f359a00a0
ChiemekaAnunkor <anunkorjr@gmail.com>
2022-12-07 00:56:50 -0600

    updated components

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./components/footer";
+import Navbar from "./components/navbar";
+import Weather from "./components/weather";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Weather />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
