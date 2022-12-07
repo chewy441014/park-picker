73514f8822ebf346ef78aff3cdb17715dd927cf4
David Codner <dccodner24@gmail.com>
2022-12-07 11:57:42 -0600

    Added on to models, more vitruals needed for user's full name to be displayed. Starting seed.js

---
+++ server/seeds/seed.js
@@ -0,0 +1,30 @@
+const connection = require('../config/connection');
+const { User } = require('../models');
+
+const users = [
+    {
+        "username": "David",
+        "email": "dccodner1@email.com",
+        "password": "password123"
+    },
+    {
+        "username": "Michael",
+        "email": "dccodner1@email.com",
+        "password": "password123"
+    },
+    {
+        "username": "Penny",
+        "email": "dccodner1@email.com",
+        "password": "password123"
+    },
+    {
+        "username": "Preston",
+        "email": "dccodner1@email.com",
+        "password": "password123"
+    },
+    {
+        "username": "Chiemeka",
+        "email": "dccodner1@email.com",
+        "password": "password123"
+    },
+]
