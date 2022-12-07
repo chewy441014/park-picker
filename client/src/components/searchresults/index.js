4d079ce384f0ce36fd7d0aa3bff6a55823431da5
Preston Hill <preston.leon.hill@gmail.com>
2022-12-06 18:24:08 -0600

    added weather card default, and search results default

---
+++ client/src/components/searchresults/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function SearchResults() {
+    return (
+        <div id="results-column">
+            {/* Iterate over the results and show them in a list
+              * Using something similar to 
+              * const numbers = [1, 2, 3, 4, 5];
+              * const listItems = numbers.map((number) =>
+              *   <li>{number}</li>
+              * ); 
+              * display ten search results 
+              */}
+        </div>
+    );
+}
+
+export default SearchResults
