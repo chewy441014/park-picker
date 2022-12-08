8cd77536dd9d3d6dab9dd9b394ce23e96384cc86
Michael Gostomski <gostomski.m@gmail.com>
2022-12-07 18:11:58 -0600

    added code to resolvers and typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,46 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  
+  type: User {
+    _id: ID
+    firstName: String
+    lastName: String
+    username: String
+    email: String
+    password: String
+    recentSearches: [searchSchema]!
+  }
+
+  type searchSchema {
+    searchId: ID
+    searchQuery: String
+    location: String
+    username: String
+    createdAt: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    user(username: String!): User
+    trips(username: String!): [Trip]
+    trip(tripId: ID!): Trip
+    me: User
+  }
+
+  type Mutation {
+    addUser(
+        firstName: String!, 
+        lastName: String!,
+        username: String!, 
+        email: String!, 
+        password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addTrip(userQuery: String!, userSearch: String!, createdAt: String)
+  }
 `;
 
 module.exports = typeDefs;
