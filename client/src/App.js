210a80dc300f251c095c25137b388785776fa883
David Codner <dccodner24@gmail.com>
2022-12-08 00:31:30 -0600

    Fixed merge conflicts, updated styling for footer, the styling broke when it was merged with the home page.

--- client/src/App.js
+++ client/src/App.js
@@ -89,7 +89,7 @@ function App() {
             </Routes>
 
           </div>
-          <Footer />
+          <Footer/>
         </body>
       </Router>
     </ApolloProvider>
--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -1,29 +1,29 @@
 import React from "react"
-import "./style.css"
+import "./footer.css"
 
 function Footer() {
   return (
-    <div class=" footerContainer">
-      <footer class="row mb-2 row-cols-2">
-        <div class="col">
-          <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
-            <svg class="bi me-2" width="40" height="32"></svg>
-          </a>
-          <p class="text-muted">Copy right © 2022</p>
-        </div>
+    <footer class="d-flex flex-column align-items-center bg-dark">
+      <div class="d-flex flex-column align-items-center">
+        
+        <ul class="nav d-flex flex-row mt-2">
+          <p class="text-muted mx-1">Contributors: </p>
+          <li class="nav-item mb-2 mx-1"><a href="https://github.com/dcodner24" class="nav-link p-0 text-muted">David Codner,</a></li>
+          <li class="nav-item mb-2 mx-1"><a href="https://github.com/chewy441014" class="nav-link p-0 text-muted">Preston Hill,</a></li>
+          <li class="nav-item mb-2 mx-1"><a href="https://github.com/mg8955" class="nav-link p-0 text-muted">Michael Gostomski,</a></li>
+          <li class="nav-item mb-2 mx-1"><a href="https://github.com/penelope-leung" class="nav-link p-0 text-muted">Penny Leung,</a></li>
+          <li class="nav-item mb-2 mx-1"><a href="https://github.com/ChiemekaAnunkor" class="nav-link p-0 text-muted">Chiemeka Anunkor</a></li>
+        </ul>
+      </div>
 
-        <div class="col">
-          <h5>Contributors</h5>
-          <ul class="nav flex-column adjustfooterw">
-            <li class="nav-item mb-2"><a href="https://github.com/dcodner24" class="nav-link p-0 text-muted">David Codner</a></li>
-            <li class="nav-item mb-2"><a href="https://github.com/chewy441014" class="nav-link p-0 text-muted">Preston Hill</a></li>
-            <li class="nav-item mb-2"><a href="https://github.com/mg8955" class="nav-link p-0 text-muted">Michael Gostomski</a></li>
-            <li class="nav-item mb-2"><a href="https://github.com/penelope-leung" class="nav-link p-0 text-muted">Penny Leung</a></li>
-            <li class="nav-item mb-2"><a href="https://github.com/ChiemekaAnunkor" class="nav-link p-0 text-muted">Chiemeka Anunkor</a></li>
-          </ul>
-        </div>
-      </footer>
-    </div>)
-}
+      <div class="d-flex flex-column align-items-center">
+        <p class="text-muted mb-0 mt-1">Made With 💖 & React</p>
+        <p class="text-muted mt-0">Copyright © 2022</p>
+      </div>
+
+
+    </footer>
+  )
+};
 
 export default Footer;
