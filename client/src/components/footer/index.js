4be1ed31e16ee7b46dde6481ffa1cad167918283
PenPen812 <penelope.c.leung@gmail.com>
2022-12-09 11:43:55 -0600

    styled footer

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -6,19 +6,20 @@ function Footer() {
   return (
     <footer className="d-flex flex-column align-items-center bg-dark">
       <div className="d-flex flex-column align-items-center">
-        
-        <h5 className="text-muted mx-1">Contributors <img src={GithubIcon} alt="github-repo" width={50}/><a href="https://github.com/chewy441014/park-picker"></a></h5>
+        <div class="githubIcon">
+        <h5 className="text-white mx-1 mg">Contributors <a href="https://github.com/chewy441014/park-picker"><img src={GithubIcon} alt="github-repo" width={35}/></a></h5>
+        </div>
         <ul className="nav d-flex flex-row mt-2">
-          <li className="nav-item mb-2 mx-1"><a href="https://www.linkedin.com/in/david-codner-008483251/" className="nav-link p-0 text-muted">David Codner |</a></li>
-          <li className="nav-item mb-2 mx-1"><a href="https://www.linkedin.com/in/hillpreston/" className="nav-link p-0 text-muted">Preston Hill |</a></li>
-          <li className="nav-item mb-2 mx-1"><a href="https://www.linkedin.com/in/michael-gostomski/" className="nav-link p-0 text-muted">Michael Gostomski |</a></li>
-          <li className="nav-item mb-2 mx-1"><a href="https://www.linkedin.com/in/penelope-leung/" className="nav-link p-0 text-muted">Penny Leung |</a></li>
-          <li className="nav-item mb-2 mx-1"><a href="https://www.linkedin.com/in/chiemeka-anunkor-98ba721b0/" className="nav-link p-0 text-muted">Chiemeka Anunkor</a></li>
+          <li className="nav-item mb-2 mx-1"><a href="https://www.linkedin.com/in/david-codner-008483251/" className="nav-link p-0 text-body">David Codner</a></li>
+          <li className="nav-item mb-2 mx-1"><a href="https://www.linkedin.com/in/hillpreston/" className="nav-link p-0 text-body">Preston Hill</a></li>
+          <li className="nav-item mb-2 mx-1"><a href="https://www.linkedin.com/in/michael-gostomski/" className="nav-link p-0 text-body">Michael Gostomski</a></li>
+          <li className="nav-item mb-2 mx-1"><a href="https://www.linkedin.com/in/penelope-leung/" className="nav-link p-0 text-body">Penelope Leung</a></li>
+          <li className="nav-item mb-2 mx-1"><a href="https://www.linkedin.com/in/chiemeka-anunkor-98ba721b0/" className="nav-link p-0 text-body">Chiemeka Anunkor</a></li>
         </ul>
       </div>
 
       <div className="d-flex flex-column align-items-center">
-        <p className="text-muted mb-0 mt-0">Made With 💖 & React (Copyright © 2022)</p>
+        <p className="text-white mb-0 mt-0">Made With 💖 & React (Copyright © 2022)</p>
         <p className="text-muted mt-0"></p>
       </div>
 
