a06034a8013665720bc8b469d67eaeec42a13073
David Codner <dccodner24@gmail.com>
2022-12-09 01:17:57 -0600

    Token shenanigains

--- server/models/User.js
+++ server/models/User.js
@@ -75,7 +75,7 @@ userSchema
     });
 
 
-userSchema.pre('save', async function (next) {
+userSchema.pre('validate', async function (next) {
     if (this.isNew || this.isModified('password')) {
         const saltRounds = 10;
         this.password = await bcrypt.hash(this.password, saltRounds);
