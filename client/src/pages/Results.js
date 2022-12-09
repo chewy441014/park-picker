222023049cfd9c042633d19ea7c1151f746425b1
David Codner <dccodner24@gmail.com>
2022-12-09 01:06:08 -0600

    Working in user model to enable password encyption and comparison. Reference: Mod-21 Act-26

--- client/src/pages/Results.js
+++ client/src/pages/Results.js
@@ -4,11 +4,11 @@ import { useQuery } from '@apollo/client';
 // import components
 import SearchResults from '../components/searchresults';
 
-import { QUERY_THOUGHTS } from '../utils/queries';
+
 // utils/queries needs to be modified
 
 const Results = () => {
-    const { loading, data } = useQuery(QUERY_THOUGHTS);
+
 
     return (
         <main>
