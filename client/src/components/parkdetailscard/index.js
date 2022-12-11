f6fa29930cab68a643803a7924b0a0b762d9489d
ChiemekaAnunkor <anunkorjr@gmail.com>
2022-12-10 22:04:41 -0600

    fixed park details page

--- client/src/components/parkdetailscard/index.js
+++ client/src/components/parkdetailscard/index.js
@@ -21,22 +21,25 @@ function ParkDetailsCard(props) {
 
     return (
         <div className='container rowDetails'>
-            <div className='column1'>
-                <div id="park-details">
-                    {/* Default park image, get it from the API call */}
-                    <img className='parkDimg' width={"230px"} height={"230px"} src={parkData.images[0].url} alt={parkData.images[0].altText} />
+            <div className="detailrow">
+
+                <div className='column1'>
+                    <div id="park-details">
+                        {/* Default park image, get it from the API call */}
+                        <img className='parkDimg' width={"230px"} height={"230px"} src={parkData.images[0].url} alt={parkData.images[0].altText} />
+                    </div>
                 </div>
-            </div>
-            <div className='column2'>
+                <div className='column2'>
 
-                <header>
-                    <h1 id="park-name">{parkData.fullName}</h1>
+                    <header>
+                        <h1 id="park-name">{parkData.fullName}</h1>
 
-                </header>
-                <div>
-                    <div className="column is-justify-content-center" id="park-desc">
-                        {/* Default park description, get it from the API call */}
-                        {parkData.description}
+                    </header>
+                    <div>
+                        <div className="column is-justify-content-center" id="park-desc">
+                            {/* Default park description, get it from the API call */}
+                            {parkData.description}
+                        </div>
                     </div>
                 </div>
             </div>
