87a6d6c8139d2320a4f9db34f77973f4a080dd95
Preston Hill <preston.leon.hill@gmail.com>
2022-12-10 23:08:49 -0600

    added map import script for mapquest maps

--- client/src/components/map/index.js
+++ client/src/components/map/index.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function Map(props) {
+
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = "https://api.mqcdn.com/sdk/mapquest-js/v1.3.2/mapquest.js";
+        script.async = true;
+        document.body.appendChild(script);
+      return () => {
+          document.body.removeChild(script);
+        }
+      }, []);
 
     // the first coordinate pair is park coords
     const parkLocation = props.coords[0];
