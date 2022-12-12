a434847fee04a98010ae95f05ba1c7dce18a5157
David Codner <dccodner24@gmail.com>
2022-12-11 21:35:42 -0600

    Basic profile page set up, saved searches to be set up tomorrow.

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,29 +1,22 @@
-import React, { useRef } from 'react';
-import { useQuery } from '@apollo/client';
 import ProfileCard from '../components/userProfile';
-
+import Auth from '../utils/auth';
 // import components
 
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
 
-import Auth from '../utils/auth';
+
+
 
 const Profile = () => {
-  const { username: userParam } = useRef(Auth.getProfile().data.username);
-
-  const { data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-    variables: { username: userParam },
-  });
+  
 
 
 
   // Profile card loads if user is logged in, if not message is populated
   
-  if (Auth.loggedIn() && data) {
-    console.log(data);
+  if (Auth.loggedIn()) {
+    
     return (
-      
-          <ProfileCard />
+      <ProfileCard />
       
   )
   }
