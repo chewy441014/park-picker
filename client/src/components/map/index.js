2195466dba59caf43619f86ad8117c3f49af3651
Preston Hill <preston.leon.hill@gmail.com>
2022-12-12 14:13:46 -0600

    added window tag for L, modified npm packages

--- client/src/components/map/index.js
+++ client/src/components/map/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import API from '../../utils/API';
 import './style.css';
 
@@ -7,25 +7,22 @@ function Map(props) {
   const parkLocation = props.coords[0];
   // the second coordinate pair is user location
   const userLocation = props.coords[1];
-
   let myMap;
 
   async function initMap() {
     const data = await API.getMapquest();
-    L.mapquest.key = data;
-
-    myMap = L.mapquest.map('map', {
+    window.L.mapquest.key = data;
+    myMap = window.L.mapquest.map('map', {
       center: [userLocation.lat, userLocation.lng],
-      layers: L.mapquest.tileLayer('map'),
+      layers: window.L.mapquest.tileLayer('map'),
       zoom: 12
     });
-
   }
 
   // display the map and show the directions
   function displayMap(startPoint, endPoint) {
     console.log("map updated")
-    let directions = L.mapquest.directions();
+    let directions = window.L.mapquest.directions();
     directions.route({
       start: [startPoint.lat, startPoint.lng],
       end: [endPoint.lat, endPoint.lng]
@@ -33,7 +30,14 @@ function Map(props) {
   }
 
   useEffect(async () => {
-
+    const script = document.createElement('script');
+    script.src = "https://api.mqcdn.com/sdk/mapquest-js/v1.3.2/mapquest.js";
+    const link = document.createElement('link');
+    link.href ="https://api.mqcdn.com/sdk/mapquest-js/v1.3.2/mapquest.css"
+    link.rel = "stylesheet"
+    link.type = "text/css"
+    document.head.appendChild(link)
+    document.body.appendChild(script);
     await initMap();
     displayMap(userLocation, parkLocation);
   }, []);
@@ -42,43 +46,8 @@ function Map(props) {
   return (
     <div id="map">
       {/* This is going to be a little complicated I think, this is where the map goes */}
-
     </div>
   );
 }
-
-/*
-function initMap() {
-  L.mapquest.key = 'Q87JNminvctmB5QAimcXQlzSf33AmhqY';
-  console.log("map initialized")
-  console.log([locationData.lat, locationData.lng])
-  // 'map' refers to a <div> element with the ID map
-  myMap = L.mapquest.map('map', {
-    center: [locationData.lat, locationData.lng],
-    layers: L.mapquest.tileLayer('map'),
-    zoom: 12
-  });
-}
-
-// display the map and show the directions
-function displayMap(startPoint, endPoint) {
-  console.log("map updated")
-  var directions = L.mapquest.directions();
-  directions.route({
-    start: startPoint,
-    end: endPoint
-  });
-}
-
-// Function to display Park Details
-function displayParkDetails(findIndexOf) {
-  if (myMap) {
-    myMap.remove();
-  }
-  initMap();
-  var indexOfData = parseInt(findIndexOf);
-  displayMap(userLocation, parkData.data[indexOfData].addresses[0].city + ", " + parkData.data[indexOfData].addresses[0].stateCode);
-
-*/
 
 export default Map;
